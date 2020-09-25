bd6b5858e62c560d4230076cb49ee6d6eff9405f
tsantos8080 <tsantos8080@gmail.com>
2020-09-25 11:44:52 -0300

    Criado controller Joke e metodo showJoke

---
+++ src/main/java/br/tsantos8080/jokeapp/controllers/JokeController.java
@@ -0,0 +1,23 @@
+package br.tsantos8080.jokeapp.controllers;
+
+import br.tsantos8080.jokeapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+        return "chucknorris";
+    }
+}
