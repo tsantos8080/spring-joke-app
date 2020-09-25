af36e8a41237ab7bb9fe645a403236e9ef054428
tsantos8080 <tsantos8080@gmail.com>
2020-09-25 11:27:41 -0300

    Initial commit

---
+++ src/main/java/br/tsantos8080/jokeapp/JokeappApplication.java
@@ -0,0 +1,13 @@
+package br.tsantos8080.jokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokeappApplication.class, args);
+    }
+
+}
